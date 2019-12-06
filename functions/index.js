8575a308d68b2f4caa38765cb251f261da490bc0
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-12-06 18:25:23 +0300

    fixed firebase deployment

--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ const functions = require('firebase-functions');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev, conf: {distDir: 'next'}});
+const app = next({dev, conf: {distDir: 'build'}});
 const handle = app.getRequestHandler();
 
 exports.next = functions.https.onRequest((req, res) => {
